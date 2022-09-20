630f429bace78cd51365f01a2e32abf7e9b11aa0
oldu8 <dudkoleg@gmail.com>
2022-09-20 18:44:32 -0400

    add rules page folder

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import QuizPage from './components/QuizPage/QuizPage';
 import InfoPage from './components/InfoPage/InfoPage';
 import ResultPage from './components/ResultPage/ResultPage';
+import RulesPage from './components/RulesPage/RulesPage';
 
 function App() {
 
@@ -18,7 +19,7 @@ function App() {
           <Routes>
             <Route exact path='/' element={<HomePage />}></Route>
             <Route path='/result' element={<ResultPage />}></Route>
-            <Route path='/rules' element={<div>rules page</div>}></Route>
+            <Route path='/rules' element={<RulesPage />}></Route>
             <Route path='/quiz' element={<QuizPage />}></Route>
             <Route path='/tests' element={<TestsPage />}></Route>
             <Route path='/info' element={<InfoPage />}></Route>
---
+++ src/components/RulesPage/RulesPage.js
@@ -0,0 +1,12 @@
+import styles from './RulesPage.module.scss';
+import { Link } from 'react-router-dom';
+
+function RulesPage() {
+  return (
+    <section className={styles.wrapper}>
+      This is rules page
+    </section>
+  );
+}
+
+export default RulesPage;
